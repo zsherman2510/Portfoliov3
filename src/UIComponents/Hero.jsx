564cf4b7c60749703295e323339c92767af07c61
Zavion Sherman <zsherman2510@hotmail.com>
2023-05-29 21:55:23 -0700

    revised the hero

--- src/UIComponents/Hero.jsx
+++ src/UIComponents/Hero.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className="heading">Hey, my name is</div>
-      <div className="subheader">Zavion Sherman.</div>
+      <div className="heading">Hello! 👋🏾</div>
+      <div className="subheader">I'm Zavion Sherman.</div>
       <div className="subheader grey">I build things for the web.</div>
-      <div className="info">I'm a software engineer specializing in building exceptional digital experiences. Currently I'm focused on building user experiences in React for banking products at <span className='amex'>American Express</span></div>
+      <div className="info">I'm a <span className='amex'>software engineer</span> specializing in building exceptional digital experiences and crafting innovative solutions through code and creativity</div>
+      <div className="cta">Get in touch 👉🏾 <a className='mailTo'>shermanzavion@gmail.com</a></div>
     </div>
   )
 }
