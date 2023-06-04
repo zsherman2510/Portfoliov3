53e8e5c00e81fc3b1c899c66d97191a9d9fb1ddf
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-03 20:44:30 -0700

    fixed node version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import Hero from './UIComponents/Hero'
 import Skills from './UIComponents/Skills'
 import Experience from './UIComponents/Experience'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
