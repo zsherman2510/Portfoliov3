2ecbc65d280ddd8985a4855a71ae315b02426da6
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-03 20:45:30 -0700

    fixed n

--- src/UIComponents/Hero.jsx
+++ src/UIComponents/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className="heading">Hello! 👋🏾</div>
+      <div className="heading">Hello!.. 👋🏾</div>
       <div className="subheader text-lg md:text-2xl">I'm Zavion Sherman.</div>
       <div className="subheader grey text-md md:text-xl">I build things for the web.</div>
       <div className="info">I'm a <span className='amex'>software engineer</span> specializing in building exceptional digital experiences and crafting innovative solutions through code and creativity</div>
