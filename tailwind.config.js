0756adf0f8bbc1744e65d51a6b14c43a0a6edd68
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-03 21:35:16 -0700

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      md: '1.3rem',
+      lg: '2rem',
+      base: '1rem',
+      xl: '2.8rem',
+      '2xl': '3.4rem',
+      '3xl': '4.3rem',
+    },
     extend: {},
   },
   plugins: [],
